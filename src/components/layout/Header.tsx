773bd6747d0b697f73474edd36526512489e6d95
tobiasgrundl3-web <tobiasgrundl3@gmail.com>
2026-06-08 22:35:01 +0200

    Add files via upload

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,15 +19,17 @@ export default function Header() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/images/dgd-logo.webp"
-            alt="DGD Direkt"
-            className="h-8 w-auto"
-          />
+          <a href="/">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/images/dgd-logo.webp"
+              alt="DGD Direkt"
+              className="h-6 sm:h-8 w-auto"
+            />
+          </a>
           <a
             href="#anfrage"
-            className="bg-[#d4a843] text-white px-4 py-2 rounded-lg font-semibold text-sm hover:bg-[#b8902e] transition-colors whitespace-nowrap"
+            className="bg-[#d4a843] text-white px-3 py-1.5 sm:px-4 sm:py-2 rounded-lg font-semibold text-xs sm:text-sm hover:bg-[#b8902e] transition-colors whitespace-nowrap"
           >
             Partnergespräch anfragen
           </a>
